f80629ef219036e45f5b7cf8101e844f4c118f05
shiyi <shiyi@zugeliang01.com>
2019-09-24 16:16:53 +0800

    no message

--- server/config/wechat.js
+++ server/config/wechat.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = {
     appid: 'wxf58455f0c5a38d1d', 
-    appsecret: '914d770e7224e980b94ae6727e9c2810',
+    secret: '914d770e7224e980b94ae6727e9c2810',
     accessTokenPath: path.resolve(__dirname, './accessToken.json'), 
     jsapiTicketPath: path.resolve(__dirname, './jsapiTicket.json') 
 }
