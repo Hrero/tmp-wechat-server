efa9721c54d77937f0dd42117bbcf5d342da2432
shiyi <shiyi@zugeliang01.com>
2019-09-24 13:55:56 +0800

    vant

--- src/main.js
+++ src/main.js
@@ -9,6 +9,10 @@ import dateFormat from '@/vueFuntion/dateFormat';
 import { Lazyload , Button, Search, Loading, Swipe, SwipeItem, Tab, Tabs, RadioGroup, Radio, SwitchCell, DatetimePicker } from 'vant';
 import 'vant/lib/index.css';
 
+const options = {
+    error: 'https://static1.zugeliang01.com/boss/img/2d9f7620-0923-11e7-a590-edb42e297052.jpg?imageView2/0/w/150/h/150',
+    loading: 'https://static1.zugeliang01.com/boss/img/2d9f7620-0923-11e7-a590-edb42e297052.jpg?imageView2/0/w/150/h/150'
+};
 Vue.use(VueI18n)
 Vue.use(DatetimePicker);
 Vue.use(SwitchCell);
@@ -17,13 +21,9 @@ Vue.use(RadioGroup);
 Vue.use(Radio);
 Vue.use(SwipeItem);
 Vue.use(Loading);
-Vue.use(Vlf);
 Vue.use(Lazyload, options);
 Vue.use(Search);
 Vue.use(Button);
-Vue.use(filters);
-Vue.use(transComponents);
-Vue.use(VueClipboard);
 Vue.use(Tab).use(Tabs);
 
 Vue.prototype.httpService = httpService;
